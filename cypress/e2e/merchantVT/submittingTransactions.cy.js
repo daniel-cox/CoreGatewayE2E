b68fa604f986ef691c7ef2eab4feb857f625bb4a
ShreddShark <daniel.cox2685@gmail.com>
2024-03-04 20:31:07 -0600

    updated tests

--- cypress/e2e/merchantVT/submittingTransactions.cy.js
+++ cypress/e2e/merchantVT/submittingTransactions.cy.js
@@ -1,17 +1,57 @@
-describe('submitting a transaction', () => {
-    beforeEach(() => {
-        cy.login({ username: 'dctesting2', password: 'Spring2024!' });
-    });
+describe("submitting a transaction", () => {
+  beforeEach(() => {
+    cy.intercept("POST", "/login").as("loginRequest")
+    cy.visit("https://testing.cgatewaydev.link/login")
 
-    it('should allow a user to process a transaction', () =>{
-        cy.visit('https://testing.cgatewaydev.link/virtual-terminal');
-        cy.url().should('include', 'https://testing.cgatewaydev.link/virtual-terminal');
-        cy.get('input[name="cc-payment-method-number"]').type('4000000000000002');
-        cy.get('#input-274').type('{enter}{downarrow}{downarrow}{downarrow}{enter}');
-        cy.get('#list-item-600-4').click(); 
-        cy.get('.v-menu__content').click().type('{downarrow}{downarrow}{downarrow}{downarrow}{enter}'); 
-        cy.get('.v-menu__content').type('{enter}');
-        cy.get('input[name="amount"]').type('5.00')
-        cy.get('button[type="submit"]').click();
-    });
-});
+    // User Authentication:
+    cy.get("#username", { timeout: 5000 }).type("dctesting2")
+    cy.get("#password").type("Spring2024!")
+    cy.get('button[type="submit"]').click()
+
+    cy.wait("@loginRequest")
+  })
+
+  it("should allow a user to process a transaction", () => {
+    cy.wait(3000)
+    cy.visit("https://testing.cgatewaydev.link/virtual-terminal")
+
+    // Selecting payment method number
+    cy.get('input[name="cc-payment-method-number"]').type("4000000000000002")
+    cy.wait(500)
+
+    // Selecting Exp Year
+    cy.contains("label", "Exp Year").parent(".v-select__slot").click()
+    cy.contains("div.v-list-item__content", "2027").click()
+    cy.contains("button", "Submit").click()
+    // Adding additional transactions
+    cy.get(
+      'div[role="button"][aria-haspopup="listbox"][aria-expanded="false"][aria-owns="list-126"]'
+    ).click()
+    cy.contains("div.v-list-item__title", "First Data Nashville").click()
+    cy.get("[name='amount']")
+      .type("10.00")
+      .should("have.value", "10.00")
+      .wait(300)
+    cy.get('button[type="submit"]').click()
+
+    cy.get(
+      'div[role="button"][aria-haspopup="listbox"][aria-expanded="false"][aria-owns="list-126"]'
+    ).click()
+    cy.contains("div.v-list-item__title", "First Data Omaha").click()
+    cy.get("[name='amount']")
+      .type("10.00")
+      .should("have.value", "10.00")
+      .wait(300)
+    cy.get('button[type="submit"]').click()
+
+    cy.get(
+      'div[role="button"][aria-haspopup="listbox"][aria-expanded="false"][aria-owns="list-126"]'
+    ).click()
+    cy.contains("div.v-list-item__title", "Tsys Testing").click()
+    cy.get("[name='amount']")
+      .type("10.00")
+      .should("have.value", "10.00")
+      .wait(300)
+    cy.get('button[type="submit"]').click()
+  })
+})
