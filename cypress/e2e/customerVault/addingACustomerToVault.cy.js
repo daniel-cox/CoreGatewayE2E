cd4e6f6e031a3a2a0e3b147f03f6fa873f46cb66
daniel-cox <ghostseal3@gmail.com>
2024-04-01 16:16:28 -0500

    added tests for VT

--- cypress/e2e/customerVault/addingACustomerToVault.cy.js
+++ cypress/e2e/customerVault/addingACustomerToVault.cy.js
@@ -2,49 +2,50 @@ describe("add a customer to the vault", () => {
   // General explanation: This test suite verifies the functionality of adding a new customer to the vault within your application.
 
   beforeEach(() => {
-    cy.intercept("POST", "/login").as("loginRequest");
-    cy.visit("https://testing.cgatewaydev.link/login");
+    cy.visit("https://testing.cgatewaydev.link/login")
 
     // User Authentication:
-    cy.get("#username").type("dctesting2");
-    cy.get("#password").type("Spring2024!");
-    cy.get('button[type="submit"]').click();
+    cy.get("#username").type("dctesting2")
+    cy.get("#password").type("Spring2024!")
+    cy.contains("button", "Login").click()
 
-    cy.wait("@loginRequest");
-    //Waits for the login process to complete by checking the status of the request.
-  });
+    cy.contains("button", "Daniel Cox").should("exist")
+  })
 
   it("should add a customer to the vault", () => {
-    cy.wait(3000);
+    cy.wait(3000)
 
-    cy.visit("https://testing.cgatewaydev.link/customers");
+    cy.visit("https://testing.cgatewaydev.link/customers")
 
     // Initiate Customer Creation:
-    cy.contains("a", "Add").click();
-    cy.wait(3000);
+    cy.contains("a", "Add").click()
+
     // Customer Billing Details Input:
-    cy.get('input[name="customer-address-company"]').type("Test Company");
-    cy.get('input[name="customer-address-first-name"]').type("John");
+    cy.wait(500)
+    cy.get('input[name="customer-address-company"]')
+      .should("be.visible")
+      .type("Test Company")
+    cy.get('input[name="customer-address-first-name"]').type("John")
 
     // State Selection
-    cy.get('div[role="button"]');
-    cy.get('input[id="input-173"]').should("be.visible").click({ force: true });
-    cy.wait(1000);
-    cy.get(".v-menu__content").should("be.visible");
-    cy.contains(".v-list-item__title", "Tennessee").click();
+    cy.get('div[role="button"]')
+    cy.get('[id^="input-"]').should("be.visible").click({ force: true })
+    cy.wait(1000)
+    cy.get(".v-menu__content").should("be.visible")
+    cy.contains(".v-list-item__title", "Tennessee").click()
 
     //Customer card details
-    cy.get('input[name="nickname"]').type("Main Card");
-    cy.get('input[name="cc-payment-method-number"]').type("4000000000000002");
-    cy.wait(500);
+    cy.get('input[name="nickname"]').type("Main Card")
+    cy.get('input[name="cc-payment-method-number"]').type("4000000000000002")
+    cy.wait(500)
 
     // CC Month selection
-    cy.contains("label", "Exp Month").parent(".v-select__slot").click();
-    cy.contains("div.v-list-item__content", "5").click();
+    cy.contains("label", "Exp Month").parent(".v-select__slot").click()
+    cy.contains("div.v-list-item__content", "5").click()
 
     // CC Year selection
-    cy.contains("label", "Exp Year").parent(".v-select__slot").click();
-    cy.contains("div.v-list-item__content", "2027").click();
-    cy.contains("button", "Submit").click(); //sumits the transaction to the vault
-  });
-});
+    cy.contains("label", "Exp Year").parent(".v-select__slot").click()
+    cy.contains("div.v-list-item__content", "2027").click()
+    cy.contains("button", "Submit").click() //submits the transaction to the vault
+  })
+})
--- cypress/e2e/merchantVT/submittingTransactions.cy.js
+++ cypress/e2e/merchantVT/submittingTransactions.cy.js
@@ -1,17 +1,62 @@
-describe('submitting a transaction', () => {
-    beforeEach(() => {
-        cy.login({ username: 'dctesting2', password: 'Spring2024!' });
-    });
+describe("submitting a transaction", () => {
+  beforeEach(() => {
+    cy.visit("https://testing.cgatewaydev.link/login")
 
-    it('should allow a user to process a transaction', () =>{
-        cy.visit('https://testing.cgatewaydev.link/virtual-terminal');
-        cy.url().should('include', 'https://testing.cgatewaydev.link/virtual-terminal');
-        cy.get('input[name="cc-payment-method-number"]').type('4000000000000002');
-        cy.get('#input-274').type('{enter}{downarrow}{downarrow}{downarrow}{enter}');
-        cy.get('#list-item-600-4').click(); 
-        cy.get('.v-menu__content').click().type('{downarrow}{downarrow}{downarrow}{downarrow}{enter}'); 
-        cy.get('.v-menu__content').type('{enter}');
-        cy.get('input[name="amount"]').type('5.00')
-        cy.get('button[type="submit"]').click();
-    });
-});
+    // User Authentication:
+    cy.get("#username").type("dctesting2")
+    cy.get("#password").type("Spring2024!")
+    cy.contains("button", "Login").click()
+
+    cy.contains("button", "Daniel Cox").should("exist")
+  })
+
+  it("should allow a user to process a transaction", () => {
+    cy.visit("https://testing.cgatewaydev.link/virtual-terminal")
+    cy.contains("h3.mb-3", "Billing").should("be.visible")
+
+    cy.get('[id$="-company"]').type("Mockup Company")
+    cy.get('[id$="-firstName"]').type("John")
+    cy.get('[id$="-lastName"]').type("Doe")
+    cy.get('[id$="-address1"]').type("123 Ribbon Ln")
+    cy.get('[id$="-address2"]').type("suite 983b")
+    cy.get('[id$="-city"]').type("Nashville")
+
+    cy.get('[aria-owns^="list-"]').contains("State").click({ force: true })
+
+    const listItems = document.querySelectorAll(".v-list-item__content")
+
+    // Loop through the list of states
+    // Loop through the state options to find and click "Tennessee"
+    cy.get(".v-list-item__content").each((item) => {
+      const stateName = item.text().trim()
+      if (stateName === "Tennessee") {
+        cy.wrap(item).click()
+      }
+    })
+
+    cy.get('[id$="-zip"]').type("37205")
+    cy.get('[id$="-phone"]').type("123-123-1234")
+    cy.get(".link-text")
+      .contains("Use different shipping information")
+      .should("be.visible")
+      .click()
+
+    cy.get('input[name="cc-payment-method-number"]').type("4000000000000002")
+    //select month
+    cy.get(".v-select__selection").contains(4).type(6)
+    cy.get(".v-list-item__title").contains(6).click()
+    //select year
+    cy.get(".v-select__selection").contains(2024).type(2024)
+    cy.get(".v-list-item__title").contains(2026).click()
+
+    //input cvv
+    cy.get(".cc-payment-method-cvv").type(999).click()
+
+    cy.get(".custom-field-text")
+      .contains("Drop Off Location")
+      .type("Leave in office door")
+
+    cy.get('input[name="amount"]').type("5.00")
+    cy.get('button[type="submit"]').click()
+  })
+})
