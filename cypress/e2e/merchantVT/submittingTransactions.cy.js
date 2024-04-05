d20448349ed1eca1a22968636151be8970203ad1
daniel-cox <ghostseal3@gmail.com>
2024-04-05 09:10:44 -0500

    updated transaction tests

--- cypress/e2e/merchantVT/submittingTransactions.cy.js
+++ cypress/e2e/merchantVT/submittingTransactions.cy.js
@@ -1,6 +1,5 @@
 describe("submitting a transaction", () => {
   beforeEach(() => {
-<<<<<<< HEAD
     cy.visit("https://testing.cgatewaydev.link/login")
 
     // User Authentication:
@@ -26,7 +25,6 @@ describe("submitting a transaction", () => {
 
     const listItems = document.querySelectorAll(".v-list-item__content")
 
-    // Loop through the list of states
     // Loop through the state options to find and click "Tennessee"
     cy.get(".v-list-item__content").each((item) => {
       const stateName = item.text().trim()
@@ -42,35 +40,6 @@ describe("submitting a transaction", () => {
       .should("be.visible")
       .click()
 
-    cy.get('input[name="cc-payment-method-number"]').type("4000000000000002")
-    //select month
-    cy.get(".v-select__selection").contains(4).type(6)
-    cy.get(".v-list-item__title").contains(6).click()
-    //select year
-    cy.get(".v-select__selection").contains(2024).type(2024)
-    cy.get(".v-list-item__title").contains(2026).click()
-
-    //input cvv
-    cy.get(".cc-payment-method-cvv").type(999).click()
-
-    cy.get(".custom-field-text")
-      .contains("Drop Off Location")
-      .type("Leave in office door")
-
-    cy.get('input[name="amount"]').type("5.00")
-    cy.get('button[type="submit"]').click()
-=======
-    cy.intercept("POST", "/login").as("loginRequest")
-    cy.visit("login")
-
-    cy.get("#username").type("dctesting2")
-    cy.get("#password").type("Spring2024!")
-    cy.get('button[type="submit"]').click()
-
-    cy.wait("@loginRequest").then(() => {})
-  })
-
-  it("should allow a user to process a transaction", () => {
     const terminalOptions = [
       "Elavon",
       "First Data Nashville",
@@ -78,49 +47,31 @@ describe("submitting a transaction", () => {
       "Tsys testing",
     ]
 
-    terminalOptions.forEach((terminal) => {
-      cy.visit("/virtual-terminal")
+    // Loop through each option
+    terminalOptions.forEach((option) => {
+      cy.get(".v-list-item__content").each((item) => {
+        const processorName = item.text().trim()
+        if (processorName === option) {
+          cy.wrap(item).click()
+        }
+      })
 
-      //Customer Billing Details
-      cy.wait(5000)
-        .get('input[name="customer-address-company"]')
-        .type("Mock Company")
-      cy.get('input[name="customer-address-first-name"]').type("John")
-      cy.get('input[name="customer-address-last-name"]').type("Doe")
-      cy.get('input[name="customer-address-email"]').type("testing@test.com")
-      cy.get('input[name="customer-address-address1"]').type(
-        "123 Westwind Drive"
-      )
-      cy.get('input[name="customer-address-address2"]').type("Suite 119")
-      cy.get('input[name="customer-address-city"]').type("Nashville")
-      cy.get('input[name="customer-address-zip"]').type("37250")
-      cy.get('input[name="customer-address-phone"]').type("1231231234")
-      //End Customer Billing Details
+      cy.get('input[name="cc-payment-method-number"]').type("4000000000000002")
+      //select month
+      cy.get(".v-select__selection").contains(4).type(6)
+      cy.get(".v-list-item__title").contains(6).click({ force: true })
+      //select year
+      cy.get(".v-select__selection").contains(2024).type(2024)
+      cy.get(".v-list-item__title").contains(2026).click({ force: true })
 
-      cy.get('input[name="cc-payment-method-number"], #input-')
-        .type("4000000000000002")
-        .wait(500)
+      //input cvv
+      cy.get(".v-text-field__slot").contains("CVV")
+      cy.get('input[name="cc-payment-method-cvv"]').clear().type(999).click()
 
-      cy.get(".v-select__selections").contains("Elavon").click()
-
-      cy.get(".v-list-item__title").contains(terminal).click() // Selects the desired terminal option
-
-      // cy.get('input[name='cc-payment-method-cvv']')
-      //     .type('999');
-
-      cy.get('input[name="amount"]').clear().type("5")
-      cy.log("Submit button found")
+      cy.get('input[name="amount"]').clear().type("5.00")
       cy.get('button[type="submit"]').click()
-
-      cy.wait(4000)
-      cy.get(".v-card.v-sheet.theme--light")
-        .contains(".v-card__title", "Transaction Failed")
-        .should("not.exist")
-        .log("The Transaction has failed")
-        .then(() => {
-          cy.get(".v-btn__content").contains("Close").click()
-        })
+      cy.wait(3000)
+      cy.get(".v-btn").contains("Close").click()
     })
->>>>>>> a03052b087045011eb587eac829aaac88fafbac3
   })
 })
