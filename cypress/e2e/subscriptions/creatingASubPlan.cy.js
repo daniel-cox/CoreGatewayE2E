fc2a02af190a7f7ced96fc469c8baf3217f23b7a
ShreddShark <daniel.cox2685@gmail.com>
2024-04-18 20:56:10 -0500

    uupdated subPlan test

--- cypress/e2e/subscriptions/creatingASubPlan.cy.js
+++ cypress/e2e/subscriptions/creatingASubPlan.cy.js
@@ -3,51 +3,49 @@ const payFrequency = ["Daily", "Weekly", "Monthly", "Yearly"]
 describe("Creating a subscription plan with random values", () => {
   beforeEach(() => {
     cy.visit("https://testing.cgatewaydev.link/login")
-
     cy.get("#username").type("dctesting2")
     cy.get("#password").type("Spring2024!")
     cy.contains("button", "Login").click()
-
     cy.contains("button", "Daniel Cox").should("exist")
   })
 
   beforeEach(() => {
     cy.visit("/plans")
-    cy.get(".v-btn__content").contains("Add").click() // Corrected the selector to match the button class
+    cy.get(".v-btn__content").contains("Add").click()
   })
 
   it("should create a subscription plan with random values based on pay frequency", () => {
-    // Randomly choose pay frequency
+    let counter = 1
+    const testName = `Test Plan-${counter}`
+    cy.get("input[name='planName']").type(testName)
+    counter++
+
+    //NOTE Billing Interval
+
+    //NOTE Installments
+    const randomInstallments = Math.floor(Math.random() * 12) + 1
+
+    cy.get("input[name='recurrence-installments']").type(
+      randomInstallments.toString()
+    )
+
+    const wholeNumber = Math.floor(Math.random() * 100) + 1
+    const decimalNumber = "00"
+    const randomAmount = `${wholeNumber}.${decimalNumber.toString()}`
+
+    cy.get("input[name='billing-recurrence-amount']").clear().type(randomAmount)
+
+    // Choose pay frequency
     const randomPayFrequency =
       payFrequency[Math.floor(Math.random() * payFrequency.length)]
 
-    // Select the pay frequency in the dropdown
-    cy.get(".v-select__selections")
     // Click on the selected pay frequency
     cy.get(".v-select__selections").contains(randomPayFrequency).click()
 
-    // Wait for the dropdown to be fully visible and interactable
-    cy.get(".v-select__selections").should("be.visible")
-
     // Logic based on pay frequency
-    if (randomPayFrequency === randomPayFrequency) {
-      const daysOfWeek = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-      ]
-      const randomDayOfWeek =
-        daysOfWeek[Math.floor(Math.random() * daysOfWeek.length)]
-      cy.get(randomDayOfWeek).click({ force: true })
-    } else if (randomPayFrequency === "Monthly") {
+    if (randomPayFrequency === "Monthly") {
       const randomDayOfMonth = Math.floor(Math.random() * 30) + 1
-      cy.get(".v-select__selections")
-        .contains(randomDayOfMonth.toString())
-        .click({ force: true })
+      cy.contains(".v-select__menu", randomDayOfMonth.toString()).click()
     } else if (randomPayFrequency === "Yearly") {
       const months = [
         "January",
@@ -63,14 +61,48 @@ describe("Creating a subscription plan with random values", () => {
         "November",
         "December",
       ]
+
       const randomMonth = months[Math.floor(Math.random() * months.length)]
       const randomDayOfYear = Math.floor(Math.random() * 30) + 1
-      cy.get(".v-select__selections")
-        .contains(randomMonth)
-        .click({ force: true })
-      cy.get(".v-select__selections")
-        .contains(randomDayOfYear.toString())
-        .click({ force: true })
+      cy.contains(".v-select__menu", randomMonth).click()
+      cy.contains(".v-select__menu", randomDayOfYear).click()
+    } else if (randomPayFrequency === "Daily") {
+      const randomValue = Math.random()
+      const clickProbability = 0.5
+
+      if (randomValue < clickProbability) {
+        cy.get('input[name="subscription-disable-proration"]').click()
+      }
+
+      cy.get(
+        "button.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.v-size--default.secondary"
+      )
+        .contains("Save Changes")
+        .click()
+    } else if (randomPayFrequency === "Weekly") {
+      const daysOfWeek = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ]
+      if (randomValue < clickProbability) {
+        cy.get('input[name="subscription-disable-proration"]').click()
+        cy.get(
+          "button.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.v-size--default.secondary"
+        )
+          .contains("Save Changes")
+          .click()
+      } else if (randomValue > clickProbability) {
+        cy.get(
+          "button.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.v-size--default.secondary"
+        )
+          .contains("Save Changes")
+          .click()
+      }
     }
   })
 })
