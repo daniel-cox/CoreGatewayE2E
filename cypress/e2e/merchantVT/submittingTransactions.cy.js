aa9dfa8da9108e86db41a313bba3516e21f28bf8
ShreddShark <daniel.cox2685@gmail.com>
2024-05-29 08:59:48 -0500

    updated test logic for submitting a txn with various processors

--- cypress/e2e/merchantVT/submittingTransactions.cy.js
+++ cypress/e2e/merchantVT/submittingTransactions.cy.js
@@ -32,144 +32,87 @@ describe("submitting a transaction", () => {
     "Davis",
   ]
 
-  it("should allow a user to process a transaction", () => {
-    const randomFirstNameIndex = Math.floor(Math.random() * firstNames.length)
-    const randomLastNameIndex = Math.floor(Math.random() * lastNames.length)
+  const terminalOptions = [
+    "Elavon",
+    "First Data Nashville",
+    "First Data Omaha",
+    "TSYS TESTING",
+  ]
 
-    const randomFirstName = firstNames[randomFirstNameIndex]
-    const randomLastName = lastNames[randomLastNameIndex]
+  Cypress._.each(terminalOptions, (terminal, index) => {
+    it(`should allow a user to process a transaction with ${terminal}`, () => {
+      cy.visit("https://testing.cgatewaydev.link/virtual-terminal")
+      cy.wait(2000)
+      const randomFirstNameIndex = Math.floor(Math.random() * firstNames.length)
+      const randomLastNameIndex = Math.floor(Math.random() * lastNames.length)
 
-    cy.visit("/virtual-terminal")
-    cy.contains("h3.mb-3", "Billing").should("be.visible")
+      const randomFirstName = firstNames[randomFirstNameIndex]
+      const randomLastName = lastNames[randomLastNameIndex]
+      cy.wait(2000)
+      cy.visit("/virtual-terminal")
+      cy.contains("h3.mb-3", "Billing").should("be.visible")
 
-    const terminalOptions = [
-      "Elavon",
-      "First Data Nashville",
-      "First Data Omaha",
-      "TSYS TESTING",
-    ]
-    cy.wait(2000)
-    terminalOptions.forEach((processorName, index) => {
-      cy.task("log", `${index} Processing transaction with ${processorName}`)
-      let vSlots = document.querySelectorAll("v-select__selections")
-      cy.task("log", `v-slots: ${vSlots}`)
-      cy.writeFile(
-        `cypress/downloads/vSlots${index}.txt`,
-        `log, ${vSlots}v-slots: [object NodeList]`
-      )
+      cy.wait(2000)
 
-      cy.get(".v-select__slot").then(($elements) => {
-        const vSlots = $elements
-          .map((index, element) => {
-            return {
-              index: index,
-              id: element.id,
-              className: element.className,
-              innerHTML: element.innerHTML.trim().substring(0, 30),
-            }
-          })
-          .get()
+      // Input biling details
+      cy.get('[id$="-company"]').type("Mockup Company")
+      cy.get('[id$="-firstName"]').type(randomFirstName)
+      cy.get('[id$="-lastName"]').type(randomLastName)
+      cy.get('[id$="-address1"]').type("123 Ribbon Ln")
+      cy.get('[id$="-address2"]').type("suite 983b")
+      cy.get('[id$="-city"]').type("Nashville")
 
-        cy.task("log", `Processing Complete: ${JSON.stringify(vSlots)}`)
-        cy.writeFile(
-          `cypress/downloads/vSlots${index}.txt`,
-          JSON.stringify(vSlots, null, 2)
-        )
+      const listItems = document.querySelectorAll("v-list-item__content")
 
-        cy.wait(1000)
+      //select Tennessee in state dropdown
+      // Click on the state dropdown to open the menu
+      cy.get("div[data-v-09c73962]").contains("State").click({ force: true })
 
-        cy.task("log", `v-slots: ${vSlots}`)
-        // Input biling details
-        cy.get('[id$="-company"]').type("Mockup Company")
-        cy.get('[id$="-firstName"]').type(randomFirstName)
-        cy.get('[id$="-lastName"]').type(randomLastName)
-        cy.get('[id$="-address1"]').type("123 Ribbon Ln")
-        cy.get('[id$="-address2"]').type("suite 983b")
-        cy.get('[id$="-city"]').type("Nashville")
+      // Wait for the dropdown menu to open
+      cy.get(".v-menu__content").should("be.visible")
 
-        const listItems = document.querySelectorAll("v-list-item__content")
+      // Select 'Tennessee' from the dropdown menu
+      cy.contains(".v-list-item__title", "Tennessee").click()
 
-        //select Tennessee in state dropdown
-        // Click on the state dropdown to open the menu
-        cy.get("div[data-v-09c73962]").contains("State").click({ force: true })
+      cy.get('[id$="-zip"]').type("37205")
+      cy.get('[id$="-phone"]').type("123-123-1234")
+      // cy.get(".link-text")
+      //   .contains("Use different shipping information")
+      //   .should("be.visible")
+      //   .click()
 
-        // Wait for the dropdown menu to open
-        cy.get(".v-menu__content").should("be.visible")
+      //NOTE create tests to check shipping fields
 
-        // Select 'Tennessee' from the dropdown menu
-        cy.contains(".v-list-item__title", "Tennessee").click()
-
-        cy.get('[id$="-zip"]').type("37205")
-        cy.get('[id$="-phone"]').type("123-123-1234")
-        cy.get(".link-text")
-          .contains("Use different shipping information")
-          .should("be.visible")
-          .click()
-
-        //NOTE create tests to check shipping fields
-
-        //select United States in the country dropdown
-        cy.get(".v-input__control").each((item) => {
-          const countryName = item.text().trim()
-          if (countryName === "United States") {
-            cy.wrap(item).click()
-          }
-          cy.get(".v-input__control")
-        })
-
-        cy.get('input[name="cc-payment-method-number"]').type(
-          "4000000000000002"
-        )
-        //select month
-        cy.get(".v-select__selection").contains(4).type(6)
-        cy.get(".v-list-item__title").contains(6).click({ force: true })
-
-        //input cvv
-        cy.get(".v-text-field__slot").contains("CVV")
-        cy.get('input[name="cc-payment-method-cvv"]').clear().type(999).click()
-
-        // Click the dropdown to open the options
-        cy.get(".v-select__slot").eq(index).click()
-
-        // Wait for the dropdown options to appear
-        cy.get(".v-list-item__content").should("be.visible")
-
-        cy.task("log", "Waiting for Processor options....")
-
-        const item = vSlots[6]
-
-        cy.task("log", `Selecting ${item.innerHTML} processor`)
-
-        const searchForProcessor = (startIndex) => {
-          // Iterate through the vSlots array
-          for (let i = startIndex; i < vSlots.length; i++) {
-            const item = vSlots[i]
-            cy.task("log", `Selecting ${item.innerHTML} processor`)
-
-            // Search within the current vSlot element
-            cy.get(item).then(($vSlot) => {
-              const $label = $vSlot.find(`label[for="${item.id}"]`)
-              const $input = $vSlot.find(`input#${item.id}`)
-
-              if ($input.length && $label.text() === processorName) {
-                cy.wrap($label).click()
-                return
-              }
-            })
-          }
+      //select United States in the country dropdown
+      cy.get(".v-input__control").each((item) => {
+        const countryName = item.text().trim()
+        if (countryName === "United States") {
+          cy.wrap(item).click()
         }
-
-        searchForProcessor(0)
-        //input dollar amount
-        cy.wait(1000)
-        cy.get('input[name="amount"]')
-          .clear({ force: true })
-          .type("5.00", { force: true })
-        //submit Order
-        cy.get('button[type="submit"]').click()
-        cy.wait(3000)
-        cy.get(".v-btn").contains("Close").click()
+        cy.get(".v-input__control")
       })
+
+      cy.get('input[name="cc-payment-method-number"]').type("4000000000000002")
+      //select month
+      cy.get(".v-select__selection").contains(4).type(6)
+      cy.get(".v-list-item__title").contains(6).click({ force: true })
+
+      //input cvv
+      cy.get(".v-text-field__slot").contains("CVV")
+      cy.get('input[name="cc-payment-method-cvv"]').clear().type(999).click()
+
+      cy.get(".processor-terminal-dropdown").click()
+      cy.get(`#processor-${index + 1}`).click()
+      cy.wait(5000)
+
+      //input dollar amount
+      cy.get('input[name="amount"]')
+        .clear({ force: true })
+        .type("5.00", { force: true })
+      //submit Order
+      cy.get('button[type="submit"]').click()
+      cy.wait(3000)
+      cy.get(".v-btn").contains("Close").click()
     })
   })
 })
